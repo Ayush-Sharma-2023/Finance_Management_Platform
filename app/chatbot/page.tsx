68ed611aa1c609259f544253e8107c13648202bb
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-02-06 21:05:45 +0530

    modified:   app/chatbot/page.tsx

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -1,9 +1,14 @@
 import ChatBot from "@/components/ChatBot";
+import Navbar from "../../components/Navbar"
 
 export default function Home() {
   return (
+    <>
+      <Navbar />
     <div className="flex justify-center items-center h-screen">
+
       <ChatBot />
     </div>
+    </>
   );
 }
