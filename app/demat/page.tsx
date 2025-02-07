66d62345f2b6dd185854fbf46958696fa880c502
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-02-07 06:41:29 +0530

    new file:   public/equities.json

---
+++ app/demat/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar";
+import Test from "../../components/Upstox/test";
+// import Search from "@/components/Upstox/SearchEquities";
+export default function Template() {
+  return (
+    <>
+      <Navbar />
+      
+      <Test />
+      {/* <Search /> */}
+    </>
+  );
+}
---
+++ components/Upstox/config.js
@@ -0,0 +1,15 @@
+// config.js
+const config = {
+  accessToken: "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI2V0JSTFQiLCJqdGkiOiI2N2E1MzY3MjUyOWY3YTI3NjhkYTE0YjAiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaWF0IjoxNzM4ODgwNjI2LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3Mzg5NjU2MDB9.-_T_yjI_1pxSaJQEX7vCDIgSkEvj2xe0JyjD9iCdJZ8",
+
+
+
+  apiKey: "your-api-key-here",
+  baseUrl: "https://api.upstox.com/v2",
+  orderBaseUrl: "https://api-hft.upstox.com/v2",
+  instrumentKey: "NSE_INDEX|Nifty 50",
+  defaultExpiryDate: "2024-12-26"
+};
+
+export default config;
+
