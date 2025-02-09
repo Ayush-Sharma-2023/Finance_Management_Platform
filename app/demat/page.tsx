1d2f95bb35baa0eeecd2ca1de8b392812f45f779
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-02-09 22:38:01 +0530

    modified:   components/Upstox/config.js

--- app/demat/page.tsx
+++ app/demat/page.tsx
@@ -1,26 +1,26 @@
-import Navbar from "../../components/Navbar";
-import Test from "../../components/Upstox/test";
-// import Search from "@/components/Upstox/SearchEquities";
-import Holdings from "@/components/Upstox/holdings";
-import Nifty from "@/components/Upstox/nifty";
-import Funds from "@/components/Upstox/Funds";
+// import Navbar from "../../components/Navbar";
+// import Test from "../../components/Upstox/test";
+// // import Search from "@/components/Upstox/SearchEquities";
+// import Holdings from "@/components/Upstox/holdings";
+// import Nifty from "@/components/Upstox/nifty";
+// import Funds from "@/components/Upstox/Funds";
 
-export default function Template() {
-  return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Navbar />
-      <div className="container mx-auto p-6 flex flex-col items-center">
-        <div className="bg-gray-900 p-4 rounded-3xl m-4">
+// export default function Template() {
+//   return (
+//     <div className="min-h-screen bg-gray-100 text-gray-900">
+//       <Navbar />
+//       <div className="container mx-auto p-6 flex flex-col items-center">
+//         <div className="bg-gray-900 p-4 rounded-3xl m-4">
 
-      <Nifty />
-      <Funds />
-        </div>
-        <h1 className="text-3xl font-bold mb-6 text-blue-600">Purchase a Stock</h1>
-        <div className="w-full max-w-2xl bg-white p-6 rounded-2xl shadow-lg border border-gray-300">
-          <Test />
-        </div>
-        <Holdings />
-      </div>
-    </div>
-  );
-}
+//       <Nifty />
+//       <Funds />
+//         </div>
+//         <h1 className="text-3xl font-bold mb-6 text-blue-600">Purchase a Stock</h1>
+//         <div className="w-full max-w-2xl bg-white p-6 rounded-2xl shadow-lg border border-gray-300">
+//           <Test />
+//         </div>
+//         <Holdings />
+//       </div>
+//     </div>
+//   );
+// }
