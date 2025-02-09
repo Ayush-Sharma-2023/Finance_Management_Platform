1018e60a2771561767c84c4a3ce2a2e1de0ea638
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-02-09 22:49:54 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // 👈 Disables ESLint checks during build
+    ignoreDuringBuilds: true, // Ensures ESLint doesn't block production builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // (Optional) Ignore TS errors during build
   },
 };
 
