d18db4f4459b0911f6b8ae52ad85dcfdb580f1d3
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-02-09 23:43:34 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Enables static export for GitHub Pages
+  distDir: "out", // Builds the site in "out" folder
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
+  trailingSlash: true, // Fixes 404 errors on subpages
   eslint: {
     ignoreDuringBuilds: true, // Ensures ESLint doesn't block production builds
   },
